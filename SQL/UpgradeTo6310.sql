67cc85006f218505789c3094689405cd51b0ab69
Magne Rekdal <magne@rekdal.no>
2016-03-24 09:13:55 +0100

    #5488: CAVE-rapporten mangler tekst nede til venstre.

---
+++ SQL/UpgradeTo6310.sql
@@ -0,0 +1,233 @@
+SET XACT_ABORT ON;
+BEGIN TRANSACTION UpgradeTo6310
+GO
+
+PRINT 'Overall purpose: Add report for consent status NDV.'
+
+--  CREATE function GetLastEnumValuesTable, useful for many reports.
+--  CREATE table Report.TimePeriods, for a list of periods we can join to.
+--  CREATE function NDV.GetConsentStatusTable.
+--  CREATE function NDV.GetConsentSummaryTable.
+--  CREATE procedure NDV.ReportConsentStatus for report Samtykkeinnhentinger.fr3
+
+EXECUTE dbo.DbStartUpgrade 6309, 6310;
+GO
+
+PRINT '--  CREATE function GetLastEnumValuesTable, useful for many reports.'
+
+
+IF NOT OBJECT_ID('dbo.GetLastEnumValuesTable') IS NULL
+  DROP FUNCTION dbo.GetLastEnumValuesTable
+GO
+
+CREATE FUNCTION dbo.GetLastEnumValuesTable ( @ItemId INT, @EndTime DATETIME = NULL )
+RETURNS @LastEnumVals TABLE (
+  PersonId INT NOT NULL,
+  EnumVal INT NOT NULL,
+  ShortCode VARCHAR(5),
+  OptionText VARCHAR(MAX)
+) AS
+BEGIN
+  SELECT @EndTime = ISNULL(@EndTime, GETDATE() + 1);
+  INSERT @LastEnumVals
+    SELECT a.PersonId, a.EnumVal, ISNULL(mia.ShortCode, 'n/a') AS ShortCode, ISNULL(mia.OptionText, 'Ubesvart') AS OptionText
+    FROM (SELECT ce.PersonId, cdp.EnumVal, RANK()
+        OVER (
+        PARTITION BY ce.PersonId
+        ORDER BY ce.EventTime DESC) AS OrderNo
+      FROM dbo.ClinEvent ce
+      JOIN dbo.ClinDataPoint cdp
+        ON cdp.EventId = ce.EventId AND ce.EventTime < @EndTime
+      WHERE cdp.ItemId = @ItemId
+      AND ISNULL(cdp.EnumVal, -1) >= 0) a
+    LEFT JOIN dbo.MetaItemAnswer mia ON mia.ItemId = @ItemId AND mia.OrderNumber = a.EnumVal
+    WHERE a.OrderNo = 1;
+  RETURN;
+END
+GO
+
+PRINT '--  CREATE table Report.TimePeriods, for a list of periods we can join to.'
+
+-- Lag en tabell for måneder, brukes til rapportering
+SET NOCOUNT ON;
+
+IF NOT OBJECT_ID('Report.TimePeriods') IS NULL
+  DROP TABLE Report.TimePeriods
+GO
+
+CREATE TABLE Report.TimePeriods (
+  RowId INT IDENTITY(1,1) NOT NULL,
+  StartTime DATETIME NOT NULL,
+  EndTime DATETIME NOT NULL,
+  PeriodType CHAR(8) NOT NULL,
+  CONSTRAINT PK_Report_TimePeriods PRIMARY KEY (RowId )
+)
+GO
+
+CREATE UNIQUE INDEX IDX_Report_TimePeriods_StartTime_EndTime ON Report.TimePeriods( StartTime,EndTime )
+GO
+
+ALTER TABLE Report.TimePeriods ADD StartYear AS DATEPART(YYYY, StartTime)
+GO
+
+TRUNCATE TABLE Report.TimePeriods
+GO
+
+DECLARE @StartDate DATETIME
+DECLARE @EndDate DATETIME
+SET @StartDate = CAST('1900-01-01' AS DATETIME)
+WHILE @StartDate < '2100-01-01'
+BEGIN
+  SET @EndDate = DATEADD(MILLISECOND, -100, DATEADD(MM, 1, @StartDate))
+  INSERT INTO Report.TimePeriods (StartTime, EndTime, PeriodType)
+    VALUES (@StartDate, @EndDate, 'MONTH')
+  SET @StartDate = DATEADD(MM, 1, @StartDate);
+END
+
+SET @StartDate = CAST('1900-01-01' AS DATETIME)
+WHILE @StartDate < '2100-01-01'
+BEGIN
+  SET @EndDate = DATEADD(MILLISECOND, -100, DATEADD(YEAR, 1, @StartDate))
+  INSERT INTO Report.TimePeriods (StartTime, EndTime, PeriodType)
+    VALUES (@StartDate, @EndDate, 'YEAR')
+  SET @StartDate = DATEADD(YEAR, 1, @StartDate);
+END
+
+SET @StartDate = CAST('1900-01-01' AS DATETIME)
+WHILE @StartDate < '2100-01-01'
+BEGIN
+  SET @EndDate = DATEADD(MILLISECOND, -100, DATEADD(MM, 3, @StartDate))
+  INSERT INTO Report.TimePeriods (StartTime, EndTime, PeriodType)
+    VALUES (@StartDate, @EndDate, 'QUARTER')
+  SET @StartDate = DATEADD(MM, 3, @StartDate);
+END
+
+SET @StartDate = CAST('1900-01-01' AS DATETIME)
+WHILE @StartDate < '2100-01-01'
+BEGIN
+  SET @EndDate = DATEADD(MILLISECOND, -100, DATEADD(MM, 4, @StartDate))
+  INSERT INTO Report.TimePeriods (StartTime, EndTime, PeriodType)
+    VALUES (@StartDate, @EndDate, 'TERTIAL')
+  SET @StartDate = DATEADD(MM, 4, @StartDate);
+END
+GO
+
+PRINT '--  CREATE function NDV.GetConsentStatusTable.'
+
+-- Hent ut samtykkestatus for en person i en gitt periode.
+
+IF NOT OBJECT_ID('NDV.GetConsentStatus') IS NULL
+  DROP FUNCTION NDV.GetConsentStatus
+GO
+
+IF NOT OBJECT_ID('NDV.GetConsentStatusTable') IS NULL
+  DROP FUNCTION NDV.GetConsentStatusTable
+GO
+
+CREATE FUNCTION NDV.GetConsentStatusTable( @StartTime DATETIME, @EndTime DATETIME )
+RETURNS @ConsentStatus TABLE (
+  PersonId INT NOT NULL,
+  EnumVal INT NOT NULL,
+  ShortCode VARCHAR(8) NOT NULL
+) AS
+BEGIN
+  INSERT @ConsentStatus
+    SELECT PersonVisits.PersonId, ISNULL(st.EnumVal, -1) AS EnumVal, ISNULL(st.ShortCode, '?') AS ShortCode
+    FROM (SELECT ce.PersonId, cdp.EnumVal, COUNT(*) AS n
+      FROM dbo.ClinEvent ce
+      JOIN dbo.ClinDataPoint cdp
+        ON cdp.EventId = ce.EventId
+      WHERE cdp.ItemId = 3196
+      AND ce.EventTime >= @StartTime
+      AND ce.EventTime < @EndTime
+      GROUP BY ce.PersonId,
+               cdp.EnumVal) PersonVisits
+    LEFT JOIN dbo.GetLastEnumValuesTable(3389, @EndTime) st ON st.PersonId = PersonVisits.PersonId;
+  RETURN
+END
+GO
+
+PRINT '--  CREATE function NDV.GetConsentSummaryTable.'
+
+-- Lag en oppsummering av samtykkene i periode
+
+IF NOT OBJECT_ID('NDV.GetConsentSummary') IS NULL
+  DROP FUNCTION NDV.GetConsentSummary
+GO
+
+IF NOT OBJECT_ID('NDV.GetConsentSummaryTable') IS NULL
+  DROP FUNCTION NDV.GetConsentSummaryTable
+GO
+
+CREATE FUNCTION NDV.GetConsentSummaryTable( @StartTime DATETIME, @EndAt DATETIME )
+RETURNS @ConsentSummary TABLE (
+  StartTime DATETIME NOT NULL,
+  EndAt DATETIME NOT NULL,
+  ShortCode VARCHAR(3) NOT NULL,
+  Antall INT
+) AS
+BEGIN
+  INSERT @ConsentSummary
+    SELECT @StartTime, @EndAt, ShortCode, COUNT(*) Ant
+    FROM NDV.GetConsentStatusTable(@StartTime, @EndAt)
+    GROUP BY ShortCode;
+  RETURN;
+END
+GO
+
+PRINT '--  CREATE procedure NDV.ReportConsentStatus for report Samtykkeinnhentinger.fr3'
+
+IF NOT OBJECT_ID('NDV.ReportConsentStatus') IS NULL
+  DROP PROCEDURE NDV.ReportConsentStatus
+GO
+
+CREATE PROCEDURE NDV.ReportConsentStatus ( @StartTime DATETIME = NULL, @EndTime DATETIME = NULL, @PeriodType CHAR(8) = 'MONTH' ) AS
+BEGIN
+  SET NOCOUNT ON;
+  SELECT @StartTime = ISNULL(@StartTime, DATEADD(YEAR, -2, GETDATE()));
+  SELECT @EndTime = ISNULL(@EndTime, GETDATE());
+  CREATE TABLE #ResData (
+    StartTime DATETIME NOT NULL PRIMARY KEY,
+    EndTime DATETIME NOT NULL,
+    Ja INT NULL,
+    Nei INT NULL,
+    Trukket INT,
+    Ukjent INT,
+    Ubesvart INT NULL
+  );
+  ALTER TABLE #ResData ADD Totalt AS (ISNULL(Ubesvart, 0) + ISNULL(Ja, 0) + ISNULL(Nei, 0) + ISNULL(Trukket, 0) + ISNULL(Ukjent, 0));
+  ALTER TABLE #ResData ADD JaProsent AS 100.0 * Ja / (ISNULL(Ubesvart, 0) + ISNULL(Ja, 0) + ISNULL(Nei, 0) + ISNULL(Trukket, 0) + ISNULL(Ukjent, 0));
+  ALTER TABLE #ResData ADD NeiProsent AS 100.0 * Nei / (ISNULL(Ubesvart, 0) + ISNULL(Ja, 0) + ISNULL(Nei, 0) + ISNULL(Trukket, 0) + ISNULL(Ukjent, 0));
+  ALTER TABLE #ResData ADD TrukketProsent AS 100.0 * Trukket / (ISNULL(Ubesvart, 0) + ISNULL(Ja, 0) + ISNULL(Nei, 0) + ISNULL(Trukket, 0) + ISNULL(Ukjent, 0));
+  ALTER TABLE #ResData ADD UkjentProsent AS 100.0 * Ukjent / (ISNULL(Ubesvart, 0) + ISNULL(Ja, 0) + ISNULL(Nei, 0) + ISNULL(Trukket, 0) + ISNULL(Ukjent, 0));
+  ALTER TABLE #ResData ADD UbesvartProsent AS 100.0 * Ubesvart / (ISNULL(Ubesvart, 0) + ISNULL(Ja, 0) + ISNULL(Nei, 0) + ISNULL(Trukket, 0) + ISNULL(Ukjent, 0));
+  INSERT INTO #ResData (StartTime, EndTime, Ubesvart, Ja, Nei, Trukket, Ukjent)
+    SELECT ml.StartTime, ml.EndTime, (SELECT Antall
+        FROM NDV.GetConsentSummaryTable(ml.StartTime, ml.EndTime)
+        WHERE ShortCode = '?')
+      AS Ubesvart, (SELECT Antall
+        FROM NDV.GetConsentSummaryTable(ml.StartTime, ml.EndTime)
+        WHERE ShortCode = 'J')
+      AS Ja, (SELECT Antall
+        FROM NDV.GetConsentSummaryTable(ml.StartTime, ml.EndTime)
+        WHERE ShortCode = 'N')
+      AS Nei, (SELECT Antall
+        FROM NDV.GetConsentSummaryTable(ml.StartTime, ml.EndTime)
+        WHERE ShortCode = 'T')
+      AS Trukket, (SELECT Antall
+        FROM NDV.GetConsentSummaryTable(ml.StartTime, ml.EndTime)
+        WHERE ShortCode = 'U')
+      AS Ukjent
+    FROM Report.TimePeriods ml
+    WHERE (ml.StartTime >= @StartTime) AND (ml.StartTime < @EndTime AND ml.PeriodType = @PeriodType)
+  SELECT *
+  FROM #ResData
+  ORDER BY StartTime DESC;
+END
+GO
+
+EXECUTE dbo.DbFinalizeUpgrade 6310;
+GO
+
+COMMIT TRANSACTION UpgradeTo6310;
+GO
