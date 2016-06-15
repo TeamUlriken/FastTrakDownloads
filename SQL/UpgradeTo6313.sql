f2229c7ea407bb6f2e433c418d3f0080f024e143
Magne Rekdal <magne@rekdal.no>
2016-06-15 10:22:26 +0200

    Tatt med felter som beskriver skjema og yrke i spørring etter privilegier, gjør det lettere å lese datasettet.

--- SQL/UpgradeTo6313.sql
+++ SQL/UpgradeTo6313.sql
@@ -7,24 +7,26 @@ PRINT 'Overall purpose: Support profession privileges to for edit, sign and crea
 
 -- CREATE procedure dbo.GetMetaFormProfessionPrivileges to return privileges for forms based on profession.
 
-EXECUTE dbo.DbStartUpgrade	6312,6313;
-GO
-
-IF NOT OBJECT_ID('GetMetaFormProfessionPrivileges') IS NULL
-	DROP PROCEDURE GetMetaFormProfessionPrivileges
+EXECUTE dbo.DbStartUpgrade 6312,6313;
 GO
 
 PRINT '--  CREATE procedure dbo.GetMetaFormProfessionPrivileges to return privileges for forms based on profession.'
 GO
 
-CREATE PROCEDURE dbo.GetMetaFormProfessionPrivileges
-AS
+IF NOT OBJECT_ID('GetMetaFormProfessionPrivileges') IS NULL
+  DROP PROCEDURE GetMetaFormProfessionPrivileges
+GO
+
+CREATE PROCEDURE dbo.GetMetaFormProfessionPrivileges AS
 BEGIN
-	SELECT mp.ProfId, mfpp.FormId, mfpp.ProfType, mfpp.CanCreate, mfpp.CanEdit, mfpp.CanSign	
-	FROM dbo.MetaFormProfessionPrivilege mfpp
-	JOIN dbo.MetaProfession mp
-		ON mfpp.ProfType = mp.ProfType;
+  SELECT mp.ProfId, mfpp.FormId, mf.FormTitle, mfpp.ProfType, mp.ProfName, mfpp.CanCreate, mfpp.CanEdit, mfpp.CanSign	
+  FROM dbo.MetaFormProfessionPrivilege mfpp
+  JOIN dbo.MetaForm mf ON mf.FormId = mfpp.FormId
+  JOIN dbo.MetaProfession mp ON mfpp.ProfType = mp.ProfType;
 END;
+GO
+
+GRANT EXECUTE ON dbo.GetMetaFormProfessionPrivleges TO [public] AS [dbo]
 GO
 
 EXECUTE dbo.DbFinalizeUpgrade 6313;
