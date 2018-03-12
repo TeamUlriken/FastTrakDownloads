6302a0e9582cfb95b364467a522750a8f82a1051
Magne Rekdal <mrk@dips.no>
2018-03-12 17:47:38 +0100

    2017 versjon

--- FormDefinitions/NDV.bat
+++ FormDefinitions/NDV.bat
@@ -5,6 +5,7 @@ COPY Form733.xml DIAPOL_YEAR.xml
 COPY Form890.xml DIAPOL_SHORT.xml
 COPY Form791.xml DIAPOL_GRAVIDE.xml
 COPY Form534.xml DIAPOL_INSULIN.xml
+COPY Form720.xml INSULIN_PUMP1.xml
 
 ECHO Sentrale skjema er nå i 2017-versjon.
 ECHO Trykk Ctrl-C for å stoppe innkopering av 2018-versjoner
@@ -15,6 +16,7 @@ COPY Form967.xml DIAPOL_YEAR.xml
 COPY Form968.xml DIAPOL_SHORT.xml
 COPY Form925.xml DIAPOL_GRAVIDE.xml
 COPY Form953.xml DIAPOL_INSULIN.xml
+COPY Form951.xml INSULIN_PUMP1.xml
 
 ECHO Sentrale skjema er nå i 2018-versjon.
 ECHO Trykk Ctrl-C for å stoppe innkopering av 2019-versjoner
