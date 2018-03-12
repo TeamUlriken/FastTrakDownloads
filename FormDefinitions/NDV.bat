9c113cb41ce62c038ccb7f1d01abef02c35354ae
Magne Rekdal <mrk@dips.no>
2018-03-12 10:26:21 +0100

    Sentrale skjemadefinisjoner for diabetesregisteret lagt til, samt fil for å lage ulike årsversjoner. Kan brukes til å sammenligne skjema og få endringer fra år til år.

---
+++ FormDefinitions/NDV.bat
@@ -0,0 +1,23 @@
+ECHO OFF
+
+COPY Form618.xml DIAPOL_MAIN.xml
+COPY Form733.xml DIAPOL_YEAR.xml
+COPY Form890.xml DIAPOL_SHORT.xml
+COPY Form791.xml DIAPOL_GRAVIDE.xml
+COPY Form534.xml DIAPOL_INSULIN.xml
+
+ECHO Sentrale skjema er nå i 2017-versjon.
+ECHO Trykk Ctrl-C for å stoppe innkopering av 2018-versjoner
+pause
+
+COPY Form974.xml DIAPOL_MAIN.xml
+COPY Form967.xml DIAPOL_YEAR.xml
+COPY Form968.xml DIAPOL_SHORT.xml
+COPY Form925.xml DIAPOL_GRAVIDE.xml
+COPY Form953.xml DIAPOL_INSULIN.xml
+
+ECHO Sentrale skjema er nå i 2018-versjon.
+ECHO Trykk Ctrl-C for å stoppe innkopering av 2019-versjoner
+pause
+
+
