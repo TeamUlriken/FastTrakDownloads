f5948d86936ff9007cacc7f37d5b7a48a3484d0e
Magne Rekdal <mrk@dips.no>
2018-08-13 13:30:51 +0200

    Lagt til batch-fil som kan brukes for å hente ned undermapper for de ulike protokollene.

---
+++ GetAll.bat
@@ -0,0 +1,17 @@
+git clone https://bitbucket.org/emetra/bar.git BAR
+git clone https://bitbucket.org/emetra/barneovervekt.git BARNEOVERVEKT
+git clone https://bitbucket.org/emetra/cts.git CTS
+git clone https://bitbucket.org/emetra/endo.git ENDO
+git clone https://bitbucket.org/emetra/fettvest.git FettVest
+git clone https://bitbucket.org/emetra/gbd.git GBD
+git clone https://bitbucket.org/emetra/hand.git HAND
+git clone https://bitbucket.org/emetra/hofte.git HOFTE
+git clone https://bitbucket.org/emetra/lipid.git LIPID
+git clone https://bitbucket.org/emetra/ndv.git NDV
+git clone https://bitbucket.org/emetra/overvekt.git OVERVEKT
+git clone https://bitbucket.org/emetra/roas.git ROAS
+git clone https://bitbucket.org/emetra/slag.git SLAG
+rem git clone https://bitbucket.org/emetra/soreg.git SOREG
+git clone https://bitbucket.org/emetra/ultradian.git Ultradian
+git clone https://bitbucket.org/emetra/vossbar.git VOSSBAR
+git clone https://bitbucket.org/emetra/vrex.git VREX
